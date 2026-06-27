cc592223451aae15f990e06c5ef3a49c48fe7160
Prateek kotian <prateekkotian@gmail.com>
2026-06-27 19:26:56 +0900

    Initial Commit

---
+++ LocationAQI/LocationAQIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAQIApp.swift
+//  LocationAQI
+//
+//  Created by Prateek Kotian on 27/06/26.
+//
+
+import SwiftUI
+
+@main
+struct LocationAQIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
